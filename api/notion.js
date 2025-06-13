06c559da1d7bea56f57e8860b32b796b9cf50d0f
ssuvorin <svrn.sergey@gmail.com>
2025-06-14 01:45:08 +0400

    Fix API route export format for Vercel

--- api/notion.js
+++ api/notion.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Устанавливаем CORS заголовки
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -125,4 +125,4 @@ export default async function handler(req, res) {
       message: error.message
     });
   }
-} 
+}; 
